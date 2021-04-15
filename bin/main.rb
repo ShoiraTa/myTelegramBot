c40827435a026087e17ac1906c6b470c60fac357
shoirata <shoira.shakirovna@gmail.com>
2021-04-15 18:05:10 +0500

    bot initial run

---
+++ bin/main.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require  'telegram/bot'
+# require_relative '../lib/bot'
+token = "1699570578:AAH3z46KoCq__FdEKVfsG8dlt-thsdDPZuk"
+answers = ['hey', 'come']
+
+Telegram::Bot::Client.run(token) do |bot|
+     bot.listen do |message|
+          case message.text
+               when /start/i
+                    bot.api.send_message(
+                         chat_id:message.chat.id,
+                         text:"Hey #{message.from.first_name}"
+                    
+                    )
+                when /stop/i
+                    bot.api.send_message(
+                         chat_id:message.chat.id,
+                         text: "hiii")
+               
+           end
+     end
+end
