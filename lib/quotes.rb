b0058fbbdf0b417cea90ea92287b131633f9594f
ShoiraTa <shoira.shakirovna@gmail.com>
2021-04-17 16:23:51 +0500

    Rspec for Quotes class

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -10,7 +10,7 @@ def a_quote
 @quote_list= @quote_list.sample
 end
 
-private
+
 def request
 uri = URI('https://type.fit/api/quotes')
 JSON.parse(Net::HTTP.get(uri))
--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,29 +1,34 @@
 require_relative '../lib/jokes'
+require_relative '../lib/quotes'
 
 describe Jokes do
     describe "sample" do
+        let (:joke){Jokes.new}
         it "returns one sample from the array of hashes" do
-        joke = Jokes.new
+
         expect(joke.jokes.sample.length).to eq(2)
         end
     end
 
-    describe "hash key" do
-        it "returns only key of the hash" do
-        joke =Jokes.new
-        result = joke.jokes.sample[:text]
-        expect(result.class).to eq(String)
-    end
+        describe "hash key" do
+            let (:joke){Jokes.new}
+            it "returns only key of the hash" do
+         
+            result = joke.jokes.sample[:text]
+            expect(result.class).to eq(String)
         end
+    end
 end
 describe Quotes do
-describe "returns one sample" do
-let(:quote) {Quotes.new}
-it "returns a sample from the array of hashes"do
-expect()
-
-end
-end
-
-
+    describe "random quote" do
+        let(:quote){Quotes.new}
+        let(:a_quote) {quote.a_quote}
+        let(:json){quote.request}
+        it "should return a Hash"do
+        expect(a_quote.class).to eql(Hash)
+        end
+        it "returns a non empty JSON object" do
+        expect(json.class).to eql(Array)
+        end
+    end
 end
