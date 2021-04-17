0e1e2fa4f125b04eacfe22fb7864ffab60285edf
ShoiraTa <shoira.shakirovna@gmail.com>
2021-04-17 12:44:05 +0500

    add quotes JSON file

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -1,13 +1,16 @@
 require 'json'
-require 'net/htpp'
+require 'net/http'
 class Quotes
+    attr_reader :quote_list
 def initialize 
-@quotes = request
-end
-def quotes
-@quotes= @quote_sample
+@quote_list = request
 end
 
+def a_quote
+@quote_list= @quote_list.sample
+end
+
+private
 def request
 uri = URI('https://type.fit/api/quotes')
 JSON.parse(Net::HTTP.get(uri))
